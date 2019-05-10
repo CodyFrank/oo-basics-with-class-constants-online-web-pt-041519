2b6c13b5a66e1cec162d6ebd16b9ac4036b737ad
Cody Frank <cody.frank30@gmail.com>
2019-05-10 13:28:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,11 +4,12 @@ class Shoe
   
    BRANDS = []
   
-  
+
   def initialize(brand)
     @brand = brand
     if BRANDS.include(brand) == false
-    BRANDS << brand
+      BRANDS << brand
+    end
   end
 
   
